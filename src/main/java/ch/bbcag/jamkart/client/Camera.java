b5bd9c210b7c803429659a78ebada6f367a20b0d
Jan <jan.henke@bbcag.ch>
2021-02-17 07:56:47 +0100

    Small fixes
    Added one more pathmarker.
    Added draw function to camera class.

--- src/main/java/ch/bbcag/jamkart/client/Camera.java
+++ src/main/java/ch/bbcag/jamkart/client/Camera.java
@@ -1,6 +1,9 @@
 package ch.bbcag.jamkart.client;
 
+import ch.bbcag.jamkart.Constants;
+import ch.bbcag.jamkart.client.map.objects.ClientCar;
 import ch.bbcag.jamkart.utils.Point;
+import javafx.scene.canvas.Canvas;
 
 public class Camera {
     private Point position;
@@ -25,4 +28,8 @@ public class Camera {
         position.setY(y);
     }
 
+    public void drawCamera(ClientCar car, Canvas canvas){
+        setX(car.getPosition().getX() - (float) (canvas.getWidth() / 2) + Constants.CAR_SIZE / 2);
+        setY(car.getPosition().getY() - (float) (canvas.getHeight() / 2) + Constants.CAR_SIZE / 2);
+    }
 }
